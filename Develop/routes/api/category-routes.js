e5afa596cdb8a081d59ba4bcb2f1a3fa5f42d11f
enanesh <edna.ganesha.gr@gmail.com>
2022-12-30 17:47:01 -0600

    Changes added to category-routes.js

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -3,15 +3,25 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get('/',async (req, res) => {
   // find all categories
-
-  Category.findAll().then((CategoryData) => {
-    res.json(CategoryData);
-  });
-
   // be sure to include its associated Products
-  
+
+  try {
+    const allCategories = await Category.findAll({
+      include: [{
+        model: Product,
+        attributes: ['id', 'product_name', 'price', 'stock', 'category_id']
+      }],
+
+    });
+    res.status(200).json(allCategories);
+    
+  } catch (err) {
+    res.status(500).json(err);
+    
+  }
+
 });
 
 router.get('/:id', (req, res) => {
