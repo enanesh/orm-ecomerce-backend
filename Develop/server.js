7d332c1982db93326e222b233eb7b2a4bfe2d68b
enanesh <edna.ganesha.gr@gmail.com>
2023-01-02 16:06:53 -0600

    Creates CRUD for categories

--- Develop/server.js
+++ Develop/server.js
@@ -23,6 +23,6 @@ sequelize.sync({ force: false }).then(() => {
 
 
 
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}!`);
-});
+// app.listen(PORT, () => {
+//   console.log(`App listening on port ${PORT}!`);
+// });
