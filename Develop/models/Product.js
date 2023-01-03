555ab59ce8bb6960277345fec101265b8b03b917
enanesh <edna.ganesha.gr@gmail.com>
2023-01-03 15:05:18 -0600

    Bug fixed from not found require sequilize

--- Develop/models/Product.js
+++ Develop/models/Product.js
@@ -1,6 +1,6 @@
 // import important parts of sequelize library
 const { Model, DataTypes } = require('sequelize');
-//const { DELETE } = require('sequelize/types/query-types');
+const { DELETE } = require('sequelize/types/query-types');
 // import our database connection from config.js
 const sequelize = require('../config/connection');
 
